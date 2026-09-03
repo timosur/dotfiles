1d08063295c76187b199566c33125767871a2db1
Timo Surmann <timo.surmann@rwe.com>
2026-09-03 15:20:51 +0200

    chore: restore rbw credentials

--- apply.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-read -r -s -p "Enpass vault password: " MASTERPW
-printf '\n'
-export MASTERPW
-trap 'unset MASTERPW' EXIT
-
-chezmoi apply --source "$(cd -- "$(dirname -- "$0")" && pwd)" "$@"
